0d156b911074924c9ee01103ca13b4da28a49c98
Rishi Sharma <sharma.rishix02@gmail.com>
2026-06-18 18:59:49 +0530

    added mount volume and workflow uses build sh

--- build.sh
+++ build.sh
@@ -1,2 +1,32 @@
-docker build -t python_app .
-docker run -p 80:80 -t python_app
+    #!/bin/bash                                                                                                                            
+                                                                                                                                           
+    # Exit immediately if a command fails                                                                                                  
+    set -e                                                                                                                                 
+                                                                                                                                           
+    # Define variables                                                                                                                     
+    PROJECT_DIR="$HOME/Aws-learn"                                                                                                          
+    IMAGE_NAME="tracker-api-image"                                                                                                         
+    CONTAINER_NAME="tracker-container"                                                                                                     
+                                                                                                                                           
+    echo "Navigating to project directory..."                                                                                              
+    cd $PROJECT_DIR                                                                                                                        
+                                                                                                                                           
+    echo "Pulling latest code from GitHub..."                                                                                              
+    git pull origin main                                                                                                                   
+                                                                                                                                           
+    echo "Building the Docker image..."                                                                                                    
+    sudo docker build -t $IMAGE_NAME:latest .                                                                                              
+                                                                                                                                           
+    echo "Stopping and removing old container (if exists)..."                                                                              
+    sudo docker stop $CONTAINER_NAME || true                                                                                               
+    sudo docker rm $CONTAINER_NAME || true                                                                                                 
+                                                                                                                                           
+    echo "Starting the new container with a Bind Mount (sync)..."                                                                          
+    # The -v flag syncs the EC2 folder to the container's /app folder                                                                      
+    sudo docker run -d \                                                                                                                   
+      --name $CONTAINER_NAME \                                                                                                             
+      -p 8080:80 \                                                                                                                         
+      -v $PROJECT_DIR:/app \                                                                                                               
+      $IMAGE_NAME:latest                                                                                                                   
+                                                                                                                                           
+    echo "Deployment complete!"  
